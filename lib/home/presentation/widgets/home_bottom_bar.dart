f9b51867c6e62717cce0f25a14e718df92e37d14
Menelick Grandisson <mgrandisson@exomind.fr>
2023-02-07 08:38:15 +0100

    FEAT : Creates a theme for bottom bar

--- lib/home/presentation/widgets/home_bottom_bar.dart
+++ lib/home/presentation/widgets/home_bottom_bar.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:mlk_ja/common/theme/colours.dart';
 
 class HomeBottomBar extends BottomNavigationBar {
   final int index;
@@ -37,31 +36,4 @@ class HomeBottomBar extends BottomNavigationBar {
 
   @override
   ValueChanged<int>? get onTap => (int i) => function(i);
-
-  @override
-  Color? get backgroundColor => Colors.transparent;
-
-  @override
-  BottomNavigationBarType? get type => BottomNavigationBarType.fixed;
-
-  @override
-  double? get elevation => 0;
-
-  @override
-  Color? get unselectedItemColor => Colours.grey;
-
-  @override
-  Color? get selectedItemColor => Colors.black;
-
-  @override
-  bool? get showUnselectedLabels => false;
-
-  @override
-  bool? get showSelectedLabels => false;
-
-  @override
-  IconThemeData? get selectedIconTheme => const IconThemeData(size: 35);
-
-  @override
-  IconThemeData? get unselectedIconTheme => const IconThemeData(size: 23);
 }
